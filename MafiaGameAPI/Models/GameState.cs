33cedefb84f13f6a64ff2909869bf000c1d7234e
pduda98 <pduda.98@gmail.com>
2020-11-23 20:46:17 +0100

    enum serialization to string

--- MafiaGameAPI/Models/GameState.cs
+++ MafiaGameAPI/Models/GameState.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 using MafiaGameAPI.Enums;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
 namespace MafiaGameAPI.Models
 {
@@ -8,6 +10,7 @@ namespace MafiaGameAPI.Models
     {
         public string Id { get; set; }
         public List<UserState> UserStates { get; set; }
+        [JsonConverter(typeof(StringEnumConverter))]
         public PhaseEnum Phase { get; set; }
         public List<VoteState> VoteState { get; set; }
         public DateTime VotingStart { get; set; }
