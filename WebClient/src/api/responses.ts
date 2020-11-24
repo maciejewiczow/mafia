103c68241b69e5a7e76444419160c260511bc3c3
Mafciejewicz <melonik0.9846@gmail.com>
2020-11-24 01:31:33 +0100

    Add more models from api

--- WebClient/src/api/responses.ts
+++ WebClient/src/api/responses.ts
@@ -1,4 +1,3 @@
-
 export interface TokenResponse {
     token: string;
     expiresOn: string;
@@ -22,6 +21,63 @@ export interface User {
     id: string;
     name: string;
     roomId?: string;
+}
+
+export interface ObjectId {
+    creationTime: string;
+    increment: number;
+    machine: number;
+    pid: number;
+    timestamp: number;
+}
+
+export interface GameState {
+    id: string;
+    userStates: UserState[];
+    phase: PhaseEnum;
+    voteState: VoteState[];
+    votingStart: string;
+}
+
+export interface UserState {
+    userId: string;
+    role: RoleEnum;
+}
+
+export interface VoteState {
+    userId: string;
+    votedUserId: string;
+}
+
+export enum RoleEnum {
+    mafioso = 1,
+    ghost = 2,
+    citizen = 4,
+}
+
+export enum PhaseEnum {
+    night,
+    day,
+}
+
+export interface GameOptions {
+    maxPlayers: number;
+    phaseTime: string;
+    mafiosoCount: number;
+    isPublic: boolean;
+    visibleVotes: boolean;
+}
+
+export interface GameRoom {
+    id: ObjectId;
+    gameHistory: GameState[];
+    currentGameStateId: string;
+    name: string;
+    gameOptions: GameOptions;
+    owner: string;
+    Participants: string[];
+    isGameStarted: boolean;
+    isGameEnded: boolean;
 }
 
 export type RoomsResponse = RoomProjection[];
