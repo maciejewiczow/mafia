a9622b2a97befd2ebda2963bc643ac978562f9ef
pduda98 <pduda.98@gmail.com>
2020-11-30 21:26:05 +0100

    update adding user to groups while game is running

--- MafiaGameAPI/Hubs/ChatHub.cs
+++ MafiaGameAPI/Hubs/ChatHub.cs
@@ -6,6 +6,8 @@ using MafiaGameAPI.Helpers;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using MafiaGameAPI.Models.DTO;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace MafiaGameAPI.Hubs
 {
@@ -14,11 +16,13 @@ namespace MafiaGameAPI.Hubs
     {
         private readonly IChatService _chatService;
         private readonly IGameRoomsService _gameRoomsService;
+        private readonly IGameService _gameService;
 
-        public ChatHub(IChatService chatService, IGameRoomsService gameRoomsService)
+        public ChatHub(IChatService chatService, IGameRoomsService gameRoomsService, IGameService gameService)
         {
             _chatService = chatService;
             _gameRoomsService = gameRoomsService;
+            _gameService = gameService;
         }
 
         public async Task SendMessage(SendMessageDTO messageDTO)
@@ -31,15 +35,28 @@ namespace MafiaGameAPI.Hubs
 
         public override async Task OnConnectedAsync()
         {
-            // FIXME: sprawdzić czy gra w której jest gracz się rozpoczęła i dodać go do reszty chatów oprócz generalnego
-
             var roomId = await _gameRoomsService.GetRoomIdByUserId(Context.User.Identity.Name);
+            var room = await _gameRoomsService.GetRoomById(roomId);
+            List<string> groupNames = new List<string>();
+            if(room.IsGameStarted)
+            {
+                var state = await _gameService.GetCurrentState(roomId);
+                var userState = state.UserStates.First(u => u.UserId.Equals(Context.User.Identity.Name));
+                if((userState.Role & RoleEnum.Mafioso) != 0)
+                {
+                    groupNames.Add(IdentifiersHelper.GenerateChatGroupName(roomId, ChatTypeEnum.Mafia));
+                }
+                groupNames.Add(IdentifiersHelper.GenerateChatGroupName(roomId, ChatTypeEnum.Citizen));
+            }
             var user = await _gameRoomsService.GetUserById(Context.User.Identity.Name);
 
-            var groupName = IdentifiersHelper.GenerateChatGroupName(roomId, ChatTypeEnum.General);
+            groupNames.Add(IdentifiersHelper.GenerateChatGroupName(roomId, ChatTypeEnum.General));
 
-            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
-            await Clients.Groups(groupName).UserConnectedAsync(user);
+            foreach(string groupName in groupNames)
+            {
+                await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+                await Clients.Groups(groupName).UserConnectedAsync(user);
+            }
 
             var messages = await _chatService.GetMessagesByUserId(Context.User.Identity.Name, roomId);
             await Clients.Caller.MessagesOnConnectedAsync(messages);
